3af46405c959a5c482df032edbd476b9d6e1d498
Yuk <194586153+yuk228@users.noreply.github.com>
2025-11-24 15:09:09 +0900

    sss

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../src/generated/prisma'
+import { PrismaClient } from '@/generated/prisma'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
